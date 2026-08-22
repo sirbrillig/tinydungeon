c1b6b5841e858df099b998b5391fa89bd99e2f0e
Payton Swick <payton@foolord.com>
2026-08-22 17:41:06 -0400

    Add function to dedup directional detection

--- src/physics.rs
+++ src/physics.rs
@@ -25,30 +25,64 @@ fn move_things(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
     }
 }
 
+#[derive(Clone, Copy)]
+enum Axis {
+    X,
+    Y,
+}
+
+impl Axis {
+    fn index(self) -> usize {
+        match self {
+            Axis::X => 0,
+            Axis::Y => 1,
+        }
+    }
+}
+
+fn handle_collisions_dir(
+    thing: &Transform,
+    velocity: &mut Velocity,
+    collider: &Collider,
+    statics: &Query<(&Transform, &Collider), Without<Velocity>>,
+    delta: f32,
+    axis: Axis,
+) {
+    let index = axis.index();
+    let next = thing.translation[index] + velocity.0[index] * delta;
+    let mut pos = thing.translation.truncate();
+    pos[index] = next;
+    let bound = Aabb2d::new(pos, collider.half_size);
+    for (target, target_collider) in statics.iter() {
+        let target_bound = Aabb2d::new(target.translation.truncate(), target_collider.half_size);
+        if bound.intersects(&target_bound) {
+            velocity.0[index] = 0.0;
+        }
+    }
+}
+
 fn handle_collisions(
     mut movers: Query<(&Transform, &mut Velocity, &Collider)>,
     statics: Query<(&Transform, &Collider), Without<Velocity>>,
     time: Res<Time>,
 ) {
+    let delta = time.delta_secs();
     for (thing, mut velocity, collider) in movers.iter_mut() {
-        let next_x = thing.translation.truncate().x + velocity.0.x * time.delta_secs();
-        let bound = Aabb2d::new(vec2(next_x, thing.translation.y), collider.half_size);
-        for (target, target_collider) in statics {
-            let target_bound =
-                Aabb2d::new(target.translation.truncate(), target_collider.half_size);
-            if bound.intersects(&target_bound) {
-                velocity.0.x = 0.0;
-            }
-        }
-
-        let next_y = thing.translation.truncate().y + velocity.0.y * time.delta_secs();
-        let bound = Aabb2d::new(vec2(thing.translation.x, next_y), collider.half_size);
-        for (target, target_collider) in statics {
-            let target_bound =
-                Aabb2d::new(target.translation.truncate(), target_collider.half_size);
-            if bound.intersects(&target_bound) {
-                velocity.0.y = 0.0;
-            }
-        }
+        handle_collisions_dir(
+            thing,
+            &mut velocity,
+            collider,
+            &statics,
+            delta,
+            Axis::X,
+        );
+        handle_collisions_dir(
+            thing,
+            &mut velocity,
+            collider,
+            &statics,
+            delta,
+            Axis::Y,
+        );
     }
 }
