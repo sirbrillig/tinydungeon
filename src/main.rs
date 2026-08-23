1f94316888496b15abd82e6f91814fc8a41786a8
Payton Swick <payton@foolord.com>
2026-08-23 12:05:18 -0400

    Add very basic map data

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use avian2d::{
     dynamics::integrator::Gravity,
 };
 use bevy::prelude::*;
+use bevy_ecs_ldtk::{LdtkPlugin, LdtkWorldBundle, LevelSelection};
+use debug::DebugPlugin;
 use player::PlayerPlugin;
 use rock::RockPlugin;
-use debug::DebugPlugin;
 
 #[derive(SystemSet, Debug, Hash, Eq, PartialEq, Clone)]
 pub enum GameSet {
@@ -21,8 +22,9 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_camera);
+        app.add_systems(Startup, (setup_world, setup_camera));
         app.add_plugins((
+            LdtkPlugin,
             PlayerPlugin,
             RockPlugin,
             PhysicsPlugins::default().with_length_unit(50.0),
@@ -36,6 +38,7 @@ impl Plugin for GamePlugin {
             },
         );
         app.insert_resource(Gravity::ZERO);
+        app.insert_resource(LevelSelection::index(0));
         app.configure_sets(Update, (GameSet::Input).chain());
     }
 }
@@ -44,7 +47,15 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
+fn setup_world(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("test_map.ldtk").into(),
+        ..default()
+    });
+}
+
 fn main() {
+    DefaultPlugins.set(ImagePlugin::default_nearest());
     App::new()
         .add_plugins((DefaultPlugins, GamePlugin, PhysicsDebugPlugin))
         .run();
