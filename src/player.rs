19c1f2663b398a3b333030add564afb26db3c5cb
Payton Swick <payton@foolord.com>
2026-08-30 20:41:25 -0400

    Make player slower still

--- src/player.rs
+++ src/player.rs
@@ -10,9 +10,9 @@ use avian2d::{
 use bevy::{prelude::*, sprite::Anchor};
 use bevy_ecs_ldtk::{LdtkEntity, Worldly, app::LdtkEntityAppExt};
 
-const PLAYER_SPEED: f32 = 130.0;
-const PLAYER_JUMP_SPEED: f32 = 270.0;
-const PLAYER_JUMP_CUT_SPEED: f32 = 150.0;
+const PLAYER_SPEED: f32 = 90.0;
+const PLAYER_JUMP_SPEED: f32 = 255.0;
+const PLAYER_JUMP_CUT_SPEED: f32 = 190.0;
 const PLAYER_HEIGHT: f32 = 20.0;
 const PLAYER_HEIGHT_ANCHOR_OFFSET: f32 = 0.03;
 const PLAYER_FOOT_HEIGHT: f32 = 2.0;
