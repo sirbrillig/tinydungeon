cfe8aef0fd8651764405c1763ab64652308bcb49
Payton Swick <payton@foolord.com>
2026-09-05 13:37:47 -0400

    Use IntendedXVelocity for StopMoving

--- src/ai/tasks/stop_moving.rs
+++ src/ai/tasks/stop_moving.rs
@@ -1,8 +1,7 @@
-use avian2d::dynamics::rigid_body::LinearVelocity;
 use bevy::prelude::*;
 use bevy_behave::prelude::*;
 
-use crate::ai::AiSet;
+use crate::{ai::AiSet, movement::IntendedXVelocity};
 
 #[derive(Component, Clone)]
 pub struct StopMoving;
@@ -11,17 +10,11 @@ pub fn plugin(app: &mut App) {
     app.add_systems(Update, stop_moving.in_set(AiSet::Behavior));
 }
 
-fn stop_moving(
-    query: Query<&BehaveCtx, With<StopMoving>>,
-    mut commands: Commands,
-    mut mover_props: Query<&mut LinearVelocity>,
-) {
-    // @todo use Intended vel
+fn stop_moving(query: Query<&BehaveCtx, With<StopMoving>>, mut commands: Commands) {
     for ctx in query.iter() {
-        let Ok(mut vel) = mover_props.get_mut(ctx.target_entity()) else {
-            continue;
-        };
-        vel.x = 0.0;
+        commands
+            .entity(ctx.target_entity())
+            .insert(IntendedXVelocity(0.0));
         commands.trigger(ctx.success());
     }
 }
